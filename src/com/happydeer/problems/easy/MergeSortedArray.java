0e4d6802aa449d1c48bfdf48dad1d1c7144270e9
HAPPYDEER <wlcsqyn@qq.com>
2018-11-22 22:13:30 +0800

    copied a kind of solution about merge two sorted array

--- src/com/happydeer/problems/easy/MergeSortedArray.java
+++ src/com/happydeer/problems/easy/MergeSortedArray.java
@@ -23,4 +23,21 @@ public class MergeSortedArray {
 		}
 		Arrays.sort(nums1);
 	}
+	
+	/**
+	 * awesome
+	 * @param nums1
+	 * @param m
+	 * @param nums2
+	 * @param n
+	 */
+	public static void greatSolution(int[] nums1, int m, int[] nums2, int n) {
+		int p = m-- + n-- -1;
+		while (m >= 0 && n >= 0) {
+			nums1[p--] = nums1[m] >= nums2[n] ? nums1[m--] : nums2[n--];
+		}
+		while (n >= 0) {
+			nums1[p--] = nums2[n--];
+		}
+	}
 }
